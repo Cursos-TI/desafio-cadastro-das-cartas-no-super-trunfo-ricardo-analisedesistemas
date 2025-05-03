70e39c4bc262b5f9a043f39e870539c88c3354c4
ricardo-analisedesistemas <202504585583@alunos.estacio.br>
2025-05-03 15:59:50 +0000

    Código do desafio Super Trunfo nível NOVATO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,53 @@
 #include <stdio.h>
+#include<string.h>
+int main(int argc, char const *argv[])
+{
+    char idcarta[2]= "";//foi preferencia pessoal referenciar essa variavel ESTADO a um ID atribuido ao  campo ESTADO de cada carta cadastrada
+    char codcarta[5]= "";
+    char nomecidade[40];
+    int  pop;
+    float area;
+    float pib;
+    int ponttur;
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    printf("***************************************\n");
+    printf("CADASTRO PARA CARTAS JOGO SUPER TRUNFO\n");
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+       printf("Digite o ID da Carta: ");
+       scanf("%1s",idcarta);
+              
+       printf("Digite o Codigo da Carta: ");
+                                            
+       scanf("%4s",codcarta);
+       while (getchar() != '\n');//para limpar buffer antes da inicilização da var nomecidade
+                                 //caso contrario  o printf a seguir nao aparece como proxima opção a ser digitada pelo usuário
+       printf("Qual o nome da cidade?: ");
+       fgets(nomecidade, sizeof(nomecidade), stdin); //foi usado porque o desafio mostrou como exemplo de resultado de saída de dados "Nome da cidade" com string composta
+       nomecidade[strcspn(nomecidade, "\n")] = '\0'; //comando colocado para que o sistema não de um espaço muito grande entre o campo atual e o proximo, a fim de que seja mostrada ao usuario linha a linha a informação
+       
+       printf("Qual o numero total de habitantes?: ");
+       scanf("%d",&pop);
+       
+       printf("Qual a extensao territorial?: ");
+       scanf("%f",&area);
+       
+       printf("Qual o PIB em Bilhoes de Reais?: ");
+       scanf("%f",&pib); 
+       
+       printf("Quantos pontos turisticos ha?: ");
+       scanf("%d",&ponttur);
+      
+    printf("**********************************************\n");
 
+         printf("ID-ESTADO: %s \n",idcarta);
+         printf("Codigo da Carta: %s\n",codcarta);
+         printf("Cidade: %s\n",nomecidade);
+         printf("Populacao: %d habitantes\n",pop);
+         printf("Extensao Territorial: %.2fkm2\n",area);
+         printf("PIB: R$ %.2f Bilhoes\n",pib);
+         printf("Total de pontos turisticos: %d\n",ponttur);
+        
+    printf("**********************************************\n");   
+    printf("  CADASTRO DA CARTA EFETUADO COM SUCESSO ! ");
     return 0;
 }
